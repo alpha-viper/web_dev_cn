04f646af0927aa3514c9fdd0fc84623c17ee5d49
Sumit Mishra <sumitmishra2711@gmail.com>
2020-08-02 12:42:07 +0530

    Created a route and a controller

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express');
 const app=express();
 const port=7000;
 
+//use express routers
+app.use('/',require('./routes/index'));
+
+
 app.listen(port,function(err){
     if(err)
     {
