4f61c0c851a4587816ec1bd1f0a46ffc849acec7
Sumit Mishra <sumitmishra2711@gmail.com>
2020-08-14 16:34:19 +0530

    passport ejs beg

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express=require('express');
+const cookieParser=require('cookie-parser');
 const app=express();
 const port=7000;
+const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy')
+
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.use(express.static('./assets'))
+app.use(expressLayouts);
+
+// extract style and script from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 //use express routers
 app.use('/',require('./routes/index'));
 
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(session({
+    name:'codeial',
+    //TODO change the 
+    secret:'blahsomething',
+    saveUninitialized:'false',
+    resave:'false',
+    cookie:
+    {
+        maxAge: (1800+60+180)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(port,function(err){
     if(err)
